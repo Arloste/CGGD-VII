4ac213252aed63db30ec27dc7f6120da2015a761
Arloste <88305350+Arloste@users.noreply.github.com>
2022-05-29 17:53:48 +0300

    Lab 1.05. Add `Rasterization` and `Pixel shader` stages to `draw` method of `cg::renderer::rasterizer` (white cube)

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -41,7 +41,7 @@ void cg::renderer::rasterization_renderer::render()
 	{
 		auto processed = mul(matrix, vertex);
 		return std::make_pair(processed, vertex_data);
-	};`
+	};
 
 	rasterizer ->pixel_shader = [](cg::vertex vertex_data, float z)
 		{return cg::color{vertex_data.ambient_r, vertex_data.ambient_g, vertex_data.ambient_b};};
